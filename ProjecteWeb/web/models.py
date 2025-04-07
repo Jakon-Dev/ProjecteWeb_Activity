c64db3e74f7f5658fe7337037a43fbc850ef27df
Jordipifarre <jordi.pifarre.ribes@gmail.com>
2025-04-07 11:36:00 +0200

    Agents models.py

--- ProjecteWeb/web/models.py
+++ ProjecteWeb/web/models.py
@@ -36,3 +36,49 @@ class User(models.Model):
 
     def __str__(self):
         return f"{self.name}#{self.tag}"
+
+
+# ======================= AGENTS ==========================
+
+class AgentRole(models.Model):
+    id = models.CharField(max_length=120, primary_key=True)
+    name = models.CharField(max_length=120)
+    description = models.TextField(null=True, blank=True)
+    icon = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AgentAbility(models.Model):
+    name = models.CharField(max_length=120, primary_key=True)
+    description = models.TextField(null=True, blank=True)
+    icon = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Agent(models.Model):
+    id = models.CharField(max_length=120, primary_key=True)
+    name = models.CharField(max_length=120)
+    description = models.TextField(null=True, blank=True)
+    developerName = models.CharField(max_length=120, null=True, blank=True)
+
+    icon = models.URLField(max_length=200, blank=True, null=True)
+    icon_small = models.URLField(max_length=200, blank=True, null=True)
+    bust_image = models.URLField(max_length=200, blank=True, null=True)
+    full_image = models.URLField(max_length=200, blank=True, null=True)
+    full_image_v2 = models.URLField(max_length=200, blank=True, null=True)
+    background_image = models.URLField(max_length=200, blank=True, null=True)
+
+    role = models.ForeignKey(AgentRole, on_delete=models.CASCADE, blank=True, null=True)
+
+    ability1 = models.ForeignKey(AgentAbility, on_delete=models.CASCADE, related_name='ability1', null=True)
+    ability2 = models.ForeignKey(AgentAbility, on_delete=models.CASCADE, related_name='ability2', null=True)
+    grenade = models.ForeignKey(AgentAbility, on_delete=models.CASCADE, related_name='grenade', null=True)
+    ultimate = models.ForeignKey(AgentAbility, on_delete=models.CASCADE, related_name='ultimate', null=True)
+    passive = models.ForeignKey(AgentAbility, on_delete=models.CASCADE, related_name='passive', null=True)
+
+    def __str__(self):
+        return self.name
