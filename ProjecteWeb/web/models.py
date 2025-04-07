efe021d4afdbe971eb8730abe4f8649c67cd822f
PauOS004 <pau.ortiz.sanchez@gmail.com>
2025-04-07 11:28:33 +0200

    Commit models.py
    Co-Authored-By: GerardPueyo <189444787+GerardPueyo@users.noreply.github.com>
    Co-Authored-By: Marc Lapeña Riu <175534636+Jakon-Dev@users.noreply.github.com>
    Co-Authored-By: Jordipifarre <121049258+Jordipifarre@users.noreply.github.com>

--- ProjecteWeb/web/models.py
+++ ProjecteWeb/web/models.py
@@ -1,3 +1,38 @@
+import json
+from os import path
 from django.db import models
 
-# Create your models here.
+
+# ======================= USERS ==========================
+
+class PlayerCard(models.Model):
+    id = models.CharField(max_length=120, primary_key=True)
+    name = models.CharField(max_length=120)
+
+    icon = models.URLField(max_length=200, blank=True, null=True)
+    small_art = models.URLField(max_length=200, blank=True, null=True)
+    wide_art = models.URLField(max_length=200, blank=True, null=True)
+    large_art = models.URLField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PlayerTitle(models.Model):
+    id = models.CharField(max_length=120, primary_key=True)
+    name = models.CharField(max_length=120)
+    title = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    puuid = models.CharField(max_length=120, primary_key=True)
+    name = models.CharField(max_length=50)
+    tag = models.CharField(max_length=5)
+    card = models.ForeignKey(PlayerCard, on_delete=models.CASCADE, null=True)
+    title = models.ForeignKey(PlayerTitle, on_delete=models.CASCADE, null=True)
+    recent_matches = models.JSONField()
+
+    def __str__(self):
+        return f"{self.name}#{self.tag}"
